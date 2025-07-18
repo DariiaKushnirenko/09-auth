b80b043c95cd3e97dc044c89904de88bf1429c66
Dariia K <da23v12@gmail.com>
2025-07-18 10:33:39 +0100

    fixing client

--- app/(private routes)/notes/[id]/NoteDetails.client.tsx
+++ app/(private routes)/notes/[id]/NoteDetails.client.tsx
@@ -1,73 +1,54 @@
-"use client";
+'use client';
 
-import css from "../notes/NotesPage.module.css";
+import { useParams, useRouter } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
 
-import { useState } from "react";
-import { getNotes} from "../../../lib/api";
-import { useQuery, keepPreviousData } from "@tanstack/react-query";
-import { useDebounce } from "use-debounce";
+import { fetchNoteById } from '@/lib/api';
 
-import SearchBox from "../../../components//SearchBox/SearchBox";
-import NoteList from "../../../components//NoteList/NoteList";
-import Pagination from "../../../components//Pagination/Pagination";
-import Modal from "../../../components/Modal/Modal";
+import css from './NoteDetails.module.css';
 
-import type { NotesResponse } from "../../../types/note";
-import NoteForm from "@/components/NoteForm/NoteForm";
+const NoteDetailsClient = () => {
+  const router = useRouter();
+  const { id } = useParams<{ id: string }>();
+  const parsedId = Number(id);
 
-
-type NotesClientProps = {
-  initialData: NotesResponse;
-};
-
-export default function NotesClient({ initialData }: NotesClientProps) {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [debouncedSearchTerm] = useDebounce(searchTerm, 500);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const activeSearch = debouncedSearchTerm.trim();
-
-  const handleSearchChange = (value: string) => {
-    setSearchTerm(value);
-    setCurrentPage(1);
-  };
-
-  const { data: note, isLoading, isError } = useQuery<NotesResponse, Error>({
-    queryKey: ["notes", activeSearch, currentPage],
-    queryFn: () => getNotes({
-      page: currentPage,
-      search: activeSearch
-    },),
-    placeholderData: keepPreviousData,
-
-    initialData: initialData ?? null,
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(parsedId),
+    refetchOnMount: false,
   });
 
-  return (
-    <div className={css.app}>
-      <header className={css.toolbar}>
-        <SearchBox value={searchTerm} onChange={handleSearchChange} />
-        {note && note.totalPages > 1 && (
-          <Pagination
-            totalPages={note.totalPages}
-            currentPage={currentPage}
-            onPageChange={setCurrentPage}
-          />
-        )}
-        <button className={css.button} onClick={() => setIsModalOpen(true)}>
-          Create note +
-        </button>
-      </header>
+  const handleClickBack = () => {
+    router.back();
+  };
 
-      {isModalOpen && (
-      <Modal onClose={() => setIsModalOpen(false)}>
-          <NoteForm onClose={() => setIsModalOpen(false)}>
-          </NoteForm>
-      </Modal>
-    )}
-      {isLoading && <p>Loading...</p>}
-      {isError && <p>Request failed</p>}
-      {note && <NoteList notes={note.notes} />} 
-    </div>
+  if (isLoading) return <p>Loading, please wait...</p>;
+  if (error || !note) return <p>Something went wrong.</p>;
+
+  return (
+    <>
+      {note && (
+        <div className={css.container}>
+          <div className={css.item}>
+            <button className={css.backBtn} onClick={handleClickBack}>
+              Back
+            </button>
+
+            <div className={css.header}>
+              <h2>{note.title}</h2>
+            </div>
+            <p className={css.tag}>{note.tag}</p>
+            <p className={css.content}>{note.content}</p>
+            <p className={css.date}>{note.createdAt}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
-}
+};
+
+export default NoteDetailsClient;
